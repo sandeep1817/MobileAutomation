cfbd1878664ffab6dd07d64dd79e590a20246b40
Basireddy Sandeep <>
2019-06-14 16:54:07 +0530

    committing all

--- src/test/java/com/hdfcapp/pages/HomePage.java
+++ src/test/java/com/hdfcapp/pages/HomePage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.support.PageFactory;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.appium.java_client.pagefactory.iOSFindBy;
